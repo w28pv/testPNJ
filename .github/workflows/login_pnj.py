0fb13901d414820f82cd16ac28b15cf97e8d4ee1
w28pv <caohoangquan56@gmail.com>
2026-02-14 13:22:35 +0700

    Update login_pnj.py

--- .github/workflows/login_pnj.py
+++ .github/workflows/login_pnj.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 
 options = Options()
-options.add_argument("--headless")
+options.add_argument("--headless=new")   # bắt buộc
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 10)
